8d06ed5393df425efa3a51d65e212eb44e9f573a
devken-kentec <kennedy_tomazete@outlook.com>
2026-09-03 12:36:33 -0300

    teste09

--- src/main/java/br/com/kentec/asterion/controller/DescricaoReceitaController.java
+++ src/main/java/br/com/kentec/asterion/controller/DescricaoReceitaController.java
@@ -2,7 +2,7 @@ package br.com.kentec.asterion.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.CrossOrigin;
+//import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -11,7 +11,7 @@ import br.com.kentec.asterion.domain.DescricaoReceita;
 import br.com.kentec.asterion.service.DescricaoReceitaService;
 
 @RestController
-@CrossOrigin(origins = "${origin.cors.url}", maxAge = 3600)
+//@CrossOrigin(origins = "${origin.cors.url}", maxAge = 3600)
 @RequestMapping("/asterion/api/v1/descricaoReceita")
 public class DescricaoReceitaController {
 	
--- src/main/java/br/com/kentec/asterion/controller/MovimentoFinanceiroDespController.java
+++ src/main/java/br/com/kentec/asterion/controller/MovimentoFinanceiroDespController.java
@@ -5,7 +5,7 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.CrossOrigin;
+//import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -18,7 +18,7 @@ import br.com.kentec.asterion.domain.MovimentoFinanceiroDespesa;
 import br.com.kentec.asterion.service.MovimentoFinanceiroDespService;
 
 @RestController
-@CrossOrigin(origins = "${origin.cors.url}", maxAge = 3600)
+//@CrossOrigin(origins = "${origin.cors.url}", maxAge = 3600)
 @RequestMapping("/asterion/api/v1/movimentoFinanceiro")
 public class MovimentoFinanceiroDespController {
 	
