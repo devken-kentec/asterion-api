d6ca7ce85415f7d4b5c3444bedbc498e7f80ffbb
devken-kentec <kennedy_tomazete@outlook.com>
2026-09-04 07:51:30 -0300

    teste11

--- src/main/java/br/com/kentec/asterion/configuration/CorsConfig.java
+++ src/main/java/br/com/kentec/asterion/configuration/CorsConfig.java
@@ -1,21 +1,20 @@
 /*package br.com.kentec.asterion.configuration;
 
 import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.Profile;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
-@Profile("prod")
 public class CorsConfig implements WebMvcConfigurer {
 
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOrigins("https://asterion.kentec.com.br")
+                .allowedOrigins("*")
                 .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
                 .allowedHeaders("*")
                 .allowCredentials(true)
                 .maxAge(3600);
     }
-}*/
+}
+*/
